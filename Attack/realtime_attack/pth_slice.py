81219fcb1fc643a92230cb15d1f5bfb4355ab067
nwpuhkp <1289561319@qq.com>
2023-04-16 16:32:17 +0800

    update pickle

--- Attack/realtime_attack/pth_slice.py
+++ Attack/realtime_attack/pth_slice.py
@@ -1,3 +1,4 @@
+import pickle
 import re
 from PongDqn.dqn.wrappers import *
 from PongDqn.dqn.Network import DQN
@@ -17,8 +18,25 @@ def slice_pth(pth_path):
     for name, param in model.named_parameters():
         # print(name)  # 打印出网络的各个层
         # print(param.data.size())  # 打印出网络的各个层的参数的形状
-        torch.save(param.data, 'D:\CodeProject\Python\LiveAttackDrl\Attack\sliced_model2/' + name + '.bin')  # 将网络的各个层的参数保存为二进制文件
+        p = torch.nn.Parameter(param.data)  # 将网络的各个层的参数转换为Parameter类型
+        torch.save(p,
+                   'D:\CodeProject\Python\LiveAttackDrl\Attack\sliced_patched_model/' + name + '.bin')  # 将网络的各个层的参数保存为二进制文件
     print("done")
+
+
+# 使用pickle将model中的参数保存为序列化文件
+def pickle_model(model_path):
+    env = gym.make("PongNoFrameskip-v4")
+    env = make_env(env)
+    action_space = env.action_space
+    state_channel = 4
+    # 加载模型
+    model = DQN(in_channels=state_channel, n_actions=action_space.n)
+    model.load_state_dict(torch.load(model_path, map_location='cpu'))
+    # pickle
+    for name, param in model.named_parameters():
+        with open('D:\CodeProject\Python\LiveAttackDrl\Attack\pickled_model/' + name + '.pkl', 'wb') as f:
+            pickle.dump(param, f)
 
 
 # 将多个二进制文件合并为一个pth文件，从而验证拆分的正确性
@@ -54,7 +72,7 @@ def compare_bin(bin1, bin2):
 
 # 主函数
 if __name__ == '__main__':
-    slice_pth('/home/huangkepu/LiveAttackDrl/PongDqn/model/DQN_Pong_episode1480.pth')
+    slice_pth('D:\CodeProject\Python\LiveAttackDrl\PongDqn\model\DQN_Pong_episode20.pth')
     # merge_pth('../merged_model/merged_model.pth')
     # if compare_pth('../PongDqn/model/DQN_Pong_episode1480.pth', './sliced_model/merged_model.pth'):
     #     print('The two pth files are the same.')
@@ -63,4 +81,3 @@ if __name__ == '__main__':
     # conv1_w2 = re.compile(re.escape(conv1_w))
     # print(re.compile(re.escape(conv1_w)))
     # compare_bin('D:\CodeProject\Python\LiveAttackDrl\Attack\sliced_model2/conv1.weight.bin', 'D:\CodeProject\Python\LiveAttackDrl\Attack\sliced_model/conv1.weight.bin')
-
